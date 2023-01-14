41190dc3c2cfffb4383b1f0bc8318604ca15f2b6
Willer <wtorrico@gmail.com>
2023-01-14 07:50:35 -0400

    correcciones para frontend

--- src/main/java/com/prtf/wgta/controller/Controller.java
+++ src/main/java/com/prtf/wgta/controller/Controller.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://fedwgta.web.app")
 public class Controller {
     //List<Persona>listaPersonas = new ArrayList();
     @Autowired
