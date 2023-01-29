084b17667def0914f55921f0b85aaa1733ec0483
Willer <wtorrico@gmail.com>
2023-01-29 13:52:03 -0400

    se añade Preautorized a ExperienciaController

--- src/main/java/com/prtf/wgta/controller/ExperienciaController.java
+++ src/main/java/com/prtf/wgta/controller/ExperienciaController.java
@@ -77,13 +77,6 @@ public ResponseEntity<Experiencia> deleteExperiencia(@PathVariable int id) {
     }
 }
 }*/
-
-
-
-
-
-
-
 package com.prtf.wgta.controller;
 
 import com.prtf.wgta.Dto.dtoExperiencia;
@@ -95,6 +88,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -118,6 +112,7 @@ public class ExperienciaController {
         return new ResponseEntity(list, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoExperiencia dtoexp) {
         if (StringUtils.isBlank(dtoexp.getEmpresaEx())) {
@@ -133,20 +128,26 @@ public class ExperienciaController {
 
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoExperiencia dtoexp) {
         //Validamos is existe id
-        if (sExperiencia.existsById(id)) 
+        if (sExperiencia.existsById(id)) {
             return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
-        
+        }
+
         //Comparamos el nombre de la experiencia
-        if (sExperiencia.existsByEmpresaEx(dtoexp.getEmpresaEx()) && sExperiencia.getByEmpresaEx(dtoexp.getEmpresaEx()).get().getId() != id)
-            return new ResponseEntity(new Mensaje("Esa experiencia ya existe"), HttpStatus.BAD_REQUEST);
-        
+        //if (sExperiencia.existsByEmpresaEx(dtoexp.getEmpresaEx()) && sExperiencia.getByEmpresaEx(dtoexp.getEmpresaEx()).get().getId() != id)
+        //return new ResponseEntity(new Mensaje("Esa experiencia ya existe"), HttpStatus.BAD_REQUEST);
+        if (!sExperiencia.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        }
+
         //El campo no puede estar vacio
-        if (StringUtils.isBlank(dtoexp.getEmpresaEx())) 
+        if (StringUtils.isBlank(dtoexp.getEmpresaEx())) {
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-        
+        }
+
         Experiencia experiencia = sExperiencia.getOne(id).get();
         experiencia.setLogoEx(dtoexp.getLogoEx());
         experiencia.setUrlEx(dtoexp.getUrlEx());
@@ -156,16 +157,19 @@ public class ExperienciaController {
         experiencia.setCargoEx(dtoexp.getCargoEx());
         experiencia.setDesdeEx(dtoexp.getDesdeEx());
         experiencia.setHastaEx(dtoexp.getHastaEx());
-        
+
         sExperiencia.save(experiencia);
-        return new ResponseEntity(new Mensaje ("Experiencia actualizada"), HttpStatus.OK);
-        
+        return new ResponseEntity(new Mensaje("Experiencia actualizada"), HttpStatus.OK);
+
     }
-    public ResponseEntity<?> delete(@PathVariable("id")int id){
-    if(!sExperiencia.existsById(id))
-        return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
-    sExperiencia.delete(id);
-    
-    return new ResponseEntity(new Mensaje("Experiencia eliminada"), HttpStatus.OK);
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!sExperiencia.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        }
+        sExperiencia.delete(id);
+
+        return new ResponseEntity(new Mensaje("Experiencia eliminada"), HttpStatus.OK);
     }
 }
